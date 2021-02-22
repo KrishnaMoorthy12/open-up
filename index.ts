f9102670897fe3a0e347ecc0eba1b95cb4851533
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-02-22 21:12:24 +0530

    project structure setup and init

---
+++ index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import chalk from 'chalk';
+import Logger from 'js-logger';
+import 'dotenv/config';
+
+import { requestLogger } from './api/middlewares';
+
+const PORT = process.env.PORT || 5000;
+
+Logger.useDefaults({
+  defaultLevel: process.env.NODE_ENV === 'production' ? Logger.ERROR : Logger.INFO,
+  formatter: messages => {
+    messages.unshift(
+      `[${new Date().toLocaleDateString('en-GB')} ${new Date().toLocaleTimeString()}]`
+    );
+  }
+});
+
+const server = express();
+
+server.use(requestLogger);
+
+server.get('*', (_req, res) => {
+  res.sendStatus(200);
+});
+
+server.listen(PORT, () => {
+  Logger.info(`Server started at port ${chalk.magenta(PORT)}`);
+  Logger.info(`Listening for requests at: ${chalk.cyan(`http://127.0.0.1:${PORT}`)}`);
+});
