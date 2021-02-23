6ec705163bc023e9d94d6ed61ac76ed81b6655dd
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-02-23 15:32:28 +0530

    updated user and post schemas

--- api/models/post.model.ts
+++ api/models/post.model.ts
@@ -1,9 +1,17 @@
 import { Schema, model } from 'mongoose';
 
 const PostSchema = new Schema({
+  author: String,
+  is_anonymous: { type: Boolean, default: false },
+
   title: { type: String, required: true },
-  description: { type: String, required: true },
-  author: String
+  body: { type: String, required: true },
+
+  views: { type: Number, default: 0 },
+  upvotes: { type: Number, default: 0 },
+  downvotes: { type: Number, default: 0 },
+
+  tags: [{ type: String }]
 });
 
 export default model('post', PostSchema);
--- api/models/user.model.ts
+++ api/models/user.model.ts
@@ -3,6 +3,7 @@ import { Schema, model } from 'mongoose';
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true },
   name: { type: String, required: true },
+  bio: { type: String },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true }
 });
