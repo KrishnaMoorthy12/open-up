b593225204a54030dac4d322a667af2694244d1d
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-02-24 22:53:18 +0530

    setup auth routes [part]

--- api/controllers/auth.controller.ts
+++ api/controllers/auth.controller.ts
@@ -11,7 +11,22 @@ import { User } from '../models';
 const AUTH_SECRET = process.env.AUTH_SECRET!;
 
 export const login = async (req: Express.Request, res: Express.Response) => {
-  const { body } = req;
+  const {
+    body: { username, password }
+  } = req;
+  try {
+    const userDocument = await User.findOne({ username });
+    if (!userDocument) return res.send('User not found');
+
+    if (bcrypt.compareSync(password, userDocument.password)) {
+      const token = jwt.sign({ username }, AUTH_SECRET, { expiresIn: '24h' });
+      Logger.debug('Login successful.');
+      res.setHeader('authorization', `Bearer ${token}`);
+      return res.send(token);
+    }
+  } catch (err) {
+    Logger.debug(chalk.red('Error: '), err);
+  }
 };
 
 export const verify = async (req: Express.Request, res: Express.Response) => {
--- api/models/user.model.ts
+++ api/models/user.model.ts
@@ -1,11 +1,24 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
 
-const UserSchema = new Schema({
-  username: { type: Schema.Types.String, unique: true, required: true },
-  name: { type: Schema.Types.String, required: true },
-  bio: { type: Schema.Types.String },
-  email: { type: Schema.Types.String, unique: true, required: true },
-  password: { type: Schema.Types.String, required: true }
-});
+const UserSchema = new Schema(
+  {
+    username: { type: Schema.Types.String, unique: true, required: true },
+    name: { type: Schema.Types.String, required: true },
+    bio: { type: Schema.Types.String },
+    email: { type: Schema.Types.String, unique: true, required: true },
+    password: { type: Schema.Types.String, required: true }
+  },
+  {
+    timestamps: true
+  }
+);
 
-export default model('user', UserSchema);
+interface IUser extends Document {
+  username: string;
+  name: string;
+  bio?: string;
+  email: string;
+  password: string;
+}
+
+export default model<IUser>('user', UserSchema);
